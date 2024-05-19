419eeb6695ed4fd37b2954d63e7867b9a6c7a8d1
chaimae michich <michichchaimae@gmail.com>
2024-05-19 16:23:02 +0100

    task two way databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'udemy-practicec';
+  userName?: string;
+  valid:boolean = false;
+   onUsenameChange(){
+      if( this.userName != ''){
+        this.valid = true;
+      }
+  }
+
+  onSave (){
+     this.userName = '';
+  }
+
+  ngOnInit(): void {
+
+  }
+
 }
